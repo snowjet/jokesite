60a7b987b8c73f9adf8734c10eb2ce3e772e057c
Chris <chris@galah.gateway>
2020-05-04 18:12:22 +1000

    fix settings

--- jokesite/settings.py
+++ jokesite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'chrisapp.apps.ChrisappConfig',
+    'chrisapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
