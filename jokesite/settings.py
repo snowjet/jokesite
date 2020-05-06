91667a91c6bdff0c167ddd21fdfce12698d5f546
Chris <chris@galah.gateway>
2020-05-06 11:24:19 +1000

    postgres

--- jokesite/settings.py
+++ jokesite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,11 +75,21 @@ WSGI_APPLICATION = 'jokesite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokedb',
+        'USER': 'pguser',
+        'PASSWORD': 'pgsecret',
+#        'HOST': '',
+#        'PORT': '',
+    },
 }
 
 
