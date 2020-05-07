c1421cd08e21ff555ac13754cbf1a635f551a9c4
Chris <chris@galah.gateway>
2020-05-07 22:27:42 +1000

    fix static 2?

--- jokesite/settings.py
+++ jokesite/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  '/static/'
+STATIC_ROOT =  'static/'
 
 # Email Server Config
 EMAIL_HOST = 'smtp.telstra.com'
