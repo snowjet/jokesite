e4125b9207572ff6ab87760126e20d8d7d51463e
Chris <chris@galah.gateway>
2020-05-09 16:14:29 +1000

    start sh

--- start-application.sh
+++ start-application.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 if [ -n POSTGRESQL_SERVICE_HOST ] ; then
-    DJANGO_SETTINGS_MODULE=project.settings
+    DJANGO_SETTINGS_MODULE=jokesite.settings
     gunicorn jokesite.wsgi:application --bind 0.0.0.0:8080
 else
-    DJANGO_SETTINGS_MODULE=project.devsettings
+    DJANGO_SETTINGS_MODULE=jokesite.devsettings
     gunicorn jokesite.wsgi:application --bind 0.0.0.0:8080
 fi
