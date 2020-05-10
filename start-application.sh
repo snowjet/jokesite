3093346310c882f7daa0301b2385c86118bdbcee
Chris <chris@galah.gateway>
2020-05-10 17:23:01 +1000

    startup

--- start-application.sh
+++ start-application.sh
@@ -2,8 +2,9 @@
 
 if [ -z ${POSTGRESQL_SERVICE_HOST} ] ; then
     export DJANGO_SETTINGS_MODULE=jokesite.devsettings
-    gunicorn jokesite.wsgi:application --bind 0.0.0.0:8080
+    echo "Using development settings"
 else
     export DJANGO_SETTINGS_MODULE=jokesite.settings
-    gunicorn jokesite.wsgi:application --bind 0.0.0.0:8080
+    echo "Using Postgres: ${POSTGRESQL_SERVICE_HOST}"
 fi
+gunicorn jokesite.wsgi:application --bind 0.0.0.0:8080
